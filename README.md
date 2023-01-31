f245b9c7a5b7825ded4edaa827374e23191229fb
Kith7 <grzegorz.jaszczak@gmail.com>
2023-01-31 23:14:25 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+The app fetches news articles from HN ANP, it contains routing (https://npmjs.com/package/navigo), reducers, and it is split into many components. 
+
+Endpoint used:
+https://node-hnapi.herokuapp.com
---
+++ components/Comment.js
@@ -0,0 +1,17 @@
+//cool recursion example for nested comments
+export default function Comment(comment) {
+  const hasNestedComments = comment.comments.length > 0;
+  return `
+      <div class="nested-comments-${comment.level}">
+        <p class="comment-header">
+          ${comment.user} | ${comment.time_ago}
+        </p>
+        ${comment.content}
+        ${
+          hasNestedComments
+            ? comment.comments.map((comment) => Comment(comment)).join("")
+            : ""
+        }
+      </div>
+    `;
+}
